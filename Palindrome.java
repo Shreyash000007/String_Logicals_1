48ed8b2f2218b83fb9b97b6daee5ba2bf0c75e22
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-08-09 16:07:50 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,30 @@
-public class Palindrome{
+public class Palindrome
+{
 // prepare a Code to findout whether a String is Palindrome or not
+public void pal(String word)
+{
+    String rev="";//Empty String
+
+    String temp=word;
+ 
+    
+    for(int i=word.length()-1;i>=0;i--)
+    {
+       rev=rev+word.charAt(i);
+    }
+    if(temp.equals(rev))
+    {
+        System.out.println(temp+" is a Palindrome Number .");
+    }
+    else 
+    {
+        System.out.println(temp+" is not a Palindrome Number ,");
+    }
+}
+public static void main(String[] args) 
+{ 
+   Palindrome aobj=new Palindrome();  
+   aobj.pal("sos");
+}
+
 }
