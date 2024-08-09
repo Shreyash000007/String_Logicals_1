f347472879e7f767de237dd18a135f84cd2992e9
Shreyash000007 <shreyasharg@gmail.com>
2024-08-09 16:04:31 +0530

    Shreyash

--- PrintingEven.java
+++ PrintingEven.java
@@ -1,3 +1,28 @@
-public class PrintingEven{  
+public class PrintingEven
+{  
 // Write the Code to printEven Length Words With  their Length
+   public void even(String s)
+   {
+     String[] word=s.split(" ");
+
+     int[] wordLength=new int [word.length];
+
+     for(int i=0;i<=word.length-1;i++)
+     {
+        wordLength[i]=word[i].length();
+    }
+    for(int i=0;i<=word.length-1;i++)
+    {
+        if(wordLength[i]%2==0)
+        {
+            System.out.println(word[i] +" : "+wordLength[i]);
+        }
+    }
+   }
+   public static void main(String[] args) 
+   {
+     PrintingEven aobj=new PrintingEven();
+     String s="Christopher Nolan is the director of Oppenheimer";
+     aobj.even(s); 
+   }
 }
